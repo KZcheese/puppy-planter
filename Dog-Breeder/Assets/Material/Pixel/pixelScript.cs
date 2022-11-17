cd6505c77cc1c74cce3fb9a980093e8ca17c2482
HarryGuan22 <gxq1057001269@gmail.com>
2022-11-16 22:06:03 -0500

    Update Room assets

---
+++ Dog-Breeder/Assets/Material/Pixel/pixelScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class pixelScript : MonoBehaviour
+{
+    public Material effectMaterial;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
+ 
